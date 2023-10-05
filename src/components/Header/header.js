3bbf6d5104237c84c423623be4747e9f2840598c
Davi-Cesar <davicesar2515@gmail.com>
2023-10-05 11:50:37 -0300

    alterações no header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,21 +1,31 @@
 import "./header.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
-  return (
-    <header className="header">
-      <img src="/logo_vetorizada.svg" className="logo" alt="gif" />
+  const [isScrolled, setIsScrolled] = useState(0);
 
+  useEffect(() => {}, []);
+
+  const headerScrolled = () => {
+    setIsScrolled(window.scrolY);
+  };
+
+  return (
+    <header
+      id="myHeader"
+      className={headerScrolled > 100 ? "headerScrolled" : "header"}
+    >
+      <img src="/logo_vetorizada.svg" className="logo" alt="gif" />
       <nav>
         <ul>
           <li>
             <a href="#home">Home</a>
           </li>
           <li>
-            <a href="#lista-de-presentes">Lista de Presentes</a>
+            <a href="#lista-de-presentes">Lista de presentes</a>
           </li>
           <li>
-            <a href="#localizacao">Localização</a>
+            <a href="#localizacao">Cerimônia</a>
           </li>
         </ul>
       </nav>
