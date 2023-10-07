7774d98ebf083355a4f61beaedd904f80f01e740
Marlon Secundo <marlon_secundo@outlook.com>
2023-10-07 17:47:51 -0300

    fix: show count

--- src/pages/ListaDePresentes/controller.js
+++ src/pages/ListaDePresentes/controller.js
@@ -2,14 +2,17 @@ import React, { useMemo, useState } from "react";
 import presentes from "../../data/gitfs-data.json";
 
 function useController() {
-  const [showCount, setShowCount] = useState(4);
+  const [showCount, setShowCount] = useState(5);
 
   const onSeeMoreHandle = () => {
     setShowCount(showCount + 4);
   };
 
   const slicedGifts = useMemo(() => {
-    if (showCount >= presentes.length) return presentes;
+    if (showCount >= presentes.length) {
+      setShowCount(presentes.length);
+      return presentes;
+    }
 
     return presentes.slice(0, showCount);
   }, [showCount]);
